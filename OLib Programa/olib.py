7e9cf2aed7b3942b2b000b8672e24f9c547afd65
RexusWolf <francisjmp99@gmail.com>
2019-03-18 16:12:55 +0100

    Added titleSearch

--- OLib Programa/olib.py
+++ OLib Programa/olib.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import re
+
+print("Dime el nombre del libro a buscar")
+bookname = raw_input()
+bookname_list = list(bookname)
+for index, letra in enumerate(bookname_list):
+    if re.match(" ", letra):
+        bookname_list[index] = "+"
+bookname = ''.join(bookname_list)
+books_dic = requests.get('http://openlibrary.org/search.json?q=' + bookname).json()
+
+docs_dic = books_dic['docs']
+for doc in docs_dic:
+    if 'author_name' in doc:
+        author = "".join(doc['author_name'])
+        author = re.sub(r"(?<=\w)([A-Z])", r" | \1", author)
+        print (doc['title'] + " - " + author )
+    else:
+        print (doc['title'] + " - " + "Unknown Author")
